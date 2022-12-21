ca127825274affae19c6b456215278e050e6eb7f
ZaariAnas <a.zaari.business@gmail.com>
2022-12-21 13:48:14 +0100

    to be debugged

--- helper_func2.c
+++ helper_func2.c
@@ -6,20 +6,15 @@
 /*   By: azari <azari@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/19 09:37:56 by azari             #+#    #+#             */
-/*   Updated: 2022/12/19 11:41:21 by azari            ###   ########.fr       */
+/*   Updated: 2022/12/21 12:14:33 by azari            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "pipex.h"
 
-void ft_check_error(int tocheck, int checker)
+void ft_check_error(int tocheck)
 {
-	if(checker == -1 && checker == -1)
-	{
-		perror(0);
-		exit(0);
-	}
-	else if (checker != tocheck)
+	if(tocheck == -1)
 	{
 		perror(0);
 		exit(0);
--- pipex.h
+++ pipex.h
@@ -6,7 +6,7 @@
 /*   By: azari <azari@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/12 17:54:50 by azari             #+#    #+#             */
-/*   Updated: 2022/12/19 10:59:30 by azari            ###   ########.fr       */
+/*   Updated: 2022/12/21 12:16:13 by azari            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,13 +19,18 @@
 # include <stdio.h>
 # include <fcntl.h>
 # include <sys/wait.h>
+# include <sys/types.h>
 
-void	ft_error(int ret, int comp);
-char	**ft_cmd_split(char *cmd);
+void	ft_check_error(int tocheck);
+char*	ft_check_mypath(char** cmd, char** env);
 char	**ft_split(const char *s, char c);
-size_t	ft_strlen(const char *s);
+char**	ft_split_mypath(char** env);
+size_t	ft_strlen(const char *str);
+void	ft_first_child(int* fds, char **av, char** env);
+void	ft_second_child(int* fds, char **av, char** env);
 int		ft_strncmp(const char *str1, const char *str2, size_t n);
 char	*ft_strdup(const char *str);
 char	*ft_substr(char const *s, unsigned int start, size_t len);
 char	*ft_strjoin(const char *s1, const char *s2);
+
 #endif
